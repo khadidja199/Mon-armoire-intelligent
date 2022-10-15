665cdb09293b140647ffce676f847d53a1007bf8
amadou ABDOULAYE khadidja <80718409+khadidja199@users.noreply.github.com>
2022-10-15 10:50:22 +0200

    Update Code image shein.py

--- Code image shein.py
+++ Code image shein.py
@@ -6,9 +6,12 @@ from bs4 import BeautifulSoup
 import os
 import pandas as pd
 
-url = 'https://fr.shein.com/trends/New-in-Trends-sc-00654187.html?ici=fr_tab01navbar02&scici=navbar_WomenHomePage~~tab01navbar02~~2~~itemPicking_00654187~~~~0&src_module=topcat&src_tab_page_id=page_home1665673115546&src_identifier=fc%3DWomen%60sc%3DAUTOMNE-HIVER%202022%60tc%3D0%60oc%3D0%60ps%3Dtab01navbar02%60jc%3DitemPicking_00654187&srctype=category&userpath=category-AUTOMNE-HIVER-2022'
 
-r = requests.get(url)
-soup = BeautifulSoup(r.content, 'html.parser')
-for link in soup.find_all('img'):
-    print(link.get("data-src"))
+def url_casual_robe():
+    url_casual = 'https://fr.shein.com/Women-Clothing-c-2030.html?attr_values=Casual&ici=fr_tab01navbar03&scici=navbar_WomenHomePage~~tab01navbar03~~3~~real_2030~~~~0&src_module=topcat&src_tab_page_id=page_home1665756357043&src_identifier=fc%3DWomen%60sc%3DV%C3%8ATEMENTS%60tc%3D0%60oc%3D0%60ps%3Dtab01navbar03%60jc%3Dreal_2030&srctype=category&userpath=category-V%C3%8ATEMENTS&attr_ids=101_167&child_cat_id=1727'
+    r = requests.get(url_casual)
+    soup = BeautifulSoup(r.content, 'html.parser')
+    for link in soup.find_all('img'):
+        print(link.get("data-src"))
+        
+url_casual_robe()
